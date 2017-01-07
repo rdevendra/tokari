b640468d6a01aa8ead1b8380c7006d0d7a13ebc1
rdevendra <rahul.devendra@gmail.com>
2017-01-07 18:23:23 +0530

    [S3]: configure S3 to store product images on production.

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -13,6 +13,37 @@ Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
+
+  if Rails.env.production?
+    attachment_config = {
+        s3_credentials: {
+          access_key_id:     'AKIAIXNYHWMYYLBZWYUQ',
+          secret_access_key: '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',
+          bucket:            "toytokri.products"
+        },
+
+        storage:        :s3,
+        s3_headers:     { "Cache-Control" => "max-age=31557600" },
+        s3_protocol:    "https",
+        bucket:         "toytokri.products",
+        #s3_host_name:   'ec2.ap-south-1.amazonaws.com',
+        url:            's3.amazonaws.com',
+
+        styles: {
+          mini:     "48x48>",
+          small:    "100x100>",
+          product:  "240x240>",
+          large:    "600x600>"
+        },
+        path:           "/:class/:id/:style/:basename.:extension",
+        default_url:    "/:class/:id/:style/:basename.:extension",
+        default_style:  "product"
+    }
+
+    attachment_config.each do |key, value|
+      Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+   end
+  end
 end
 
 Spree.user_class = "Spree::User"
