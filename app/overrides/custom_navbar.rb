69a89476bd0299689c373e7b58111c5f8e9e9ce2
rdevendra <rahul.devendra@gmail.com>
2017-01-13 23:30:22 +0530

    [UI] customize the navigation header to show the regular links.

--- app/overrides/custom_navbar.rb
+++ app/overrides/custom_navbar.rb
@@ -1,9 +1,11 @@
+#replace original header with a custom footer
 Deface::Override.new(virtual_path: 'spree/shared/_header',
     name: 'remove_navbar',
     replace: 'div#spree-header',
     partial: 'shared/custom_header'
 )
 
+#add cutom footer
 Deface::Override.new(virtual_path: 'spree/layouts/spree_application',
     name: 'add_footer',
     insert_after: 'body',
