74e590236cf82db3dfc1efe3ccb933af72fe8b01
rdevendra <rahul.devendra@gmail.com>
2017-01-16 12:20:26 +0530

    [Tech] configure SMTP settings for action mailer.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "tokri_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # Action mailer settings
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:               'smtp.gmail.com',
+      port:                  587,
+      domain:                'toytokri.com',
+      user_name:             ENV["MAIL_USER_NAME"],
+      password:              ENV["MAIL_USER_PASSWORD"],
+      authentication:        'plain',
+      enable_starttls_auto:  true
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
