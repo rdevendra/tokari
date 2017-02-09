a6aabad51259dcd7afaf6ee53d4a280445eace64
rdevendra <rahul.devendra@gmail.com>
2017-02-09 22:20:12 +0530

    Add static pages and update footer links.

---
+++ app/controllers/spree/page_controller.rb
@@ -0,0 +1,5 @@
+module Spree
+  class PageController < Spree::BaseController
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,16 @@ Rails.application.routes.draw do
   # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
   mount Spree::Core::Engine, at: '/'
           # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get '/about' => 'spree/home#about', :as => :about
+  Spree::Core::Engine.add_routes do
+    get '/about' => 'page#about', :as => :about
+    get '/contact' => 'page#contact', :as => :contact
+    #get '/career' => 'page#career', :as => :career
+    #get '/media' => 'page#media', :as => :media
+    get '/faq' => 'page#faq', :as => :faq
+    get '/privacy_policy' => 'page#privacy_policy', :as => :privacy_policy
+    get '/shipping_and_return_policy' => 'page#snr_policy', :as => :shipping_and_return_policy
+    get '/terms_and_conditions' => 'page#tnc', :as => :terms_and_conditions
+
+  end
 end
